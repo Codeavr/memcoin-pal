aceb343674c4f04c15ca4a77e215be90ec045a69
Sergey Ankinovich <codeavr@ya.ru>
2018-11-12 02:28:52 +0300

    Fixed disabled balance command

--- main.py
+++ main.py
@@ -12,14 +12,16 @@ def balance(bot, update):
     response = requests.get(api_url + '/user/' + update.message.chat_id).text
     bot.send_message(chat_id=update.message.chat_id, text=response)
 
+def register_command(dispatcher, command, method):
+    handler = CommandHandler(command, method)
+    dispatcher.add_handler(handler)
+
 api_url = os.environ['APIURL']
 updater = Updater(token=os.environ['TOKEN'])
 dispatcher = updater.dispatcher
 
-start_handler = CommandHandler('start', start)
-balance_handler = CommandHandler('balance', balance)
-dispatcher.add_handler(start_handler)
-
+register_command(dispatcher, 'start', start)
+register_command(dispatcher, 'balance', balance)
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
