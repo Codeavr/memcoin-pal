49e3ced1d4e99eaf794f8dc9f8d0323bf971d70b
Sergey Ankinovich <codeavr@ya.ru>
2018-11-12 02:58:20 +0300

    Added token to headers

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ import requests
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 
+api_url = os.environ['APIURL']
+mem_token = os.environ['MEMTOKEN']
+tg_token = os.environ['TOKEN']
+
+headers = {'token': mem_token }
+
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Hello, {}, I'm memcoin pal bot, send /balance to see your balance".format(update.message.chat_id))
+    bot.send_message(chat_id=update.message.chat_id, text="Hello, I'm memcoin pal bot, send /balance to see your balance")
 
 def balance(bot, update):
-    response = requests.get(api_url + '/user/' + str(update.message.chat_id)).text
+    response = requests.get(api_url + '/user/' + str(update.message.chat_id), headers=headers).text
     bot.send_message(chat_id=update.message.chat_id, text=response)
 
 def register_command(dispatcher, command, method):
     handler = CommandHandler(command, method)
     dispatcher.add_handler(handler)
 
-api_url = os.environ['APIURL']
-updater = Updater(token=os.environ['TOKEN'])
+updater = Updater(token=tg_token)
 dispatcher = updater.dispatcher
 
 register_command(dispatcher, 'start', start)
