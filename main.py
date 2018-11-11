1ddcb554a8567075bd0b0f30cc71f2ce7e10eaa8
Sergey Ankinovich <codeavr@ya.ru>
2018-11-12 02:01:42 +0300

    Added /start and /balance samples

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import telegram
 import os
-bot = telegram.Bot(token=os.environ['TOKEN'])
-print(bot.get_me())
+import logging
+import requests
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+
+
+updater = Updater(token=os.environ['TOKEN'])
+dispatcher = updater.dispatcher
+
+start_handler = CommandHandler('start', start)
+balance_handler = CommandHandler('balance', balance)
+dispatcher.add_handler(start_handler)
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+api_url = os.environ['APIURL']
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Hello, I'm memcoin pal bot, send /balance to see your balance")
+
+def balance(bot, update):
+    response = requests.get(api_url + 'user/' + update.message.chat_id).text
+    bot.send_message(chat_id=update.message.chat_id, text=response)
